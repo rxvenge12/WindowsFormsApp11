864ddc7ababfce2c8835f97042c2b4b1dfd84f6b
Everlasting Despair <vlad.andrejkovets@gmail.com>
2024-04-17 09:04:03 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxLength = new System.Windows.Forms.TextBox();
+            this.textBoxWidth = new System.Windows.Forms.TextBox();
+            this.textBoxHeight = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.sortButton = new System.Windows.Forms.Button();
+            this.exportButton = new System.Windows.Forms.Button();
+            this.listBoxCharacteristics = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelLength = new System.Windows.Forms.Label();
+            this.labelWidth = new System.Windows.Forms.Label();
+            this.labelHeight = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxLength
+            // 
+            this.textBoxLength.Location = new System.Drawing.Point(88, 28);
+            this.textBoxLength.Name = "textBoxLength";
+            this.textBoxLength.Size = new System.Drawing.Size(100, 20);
+            this.textBoxLength.TabIndex = 0;
+            // 
+            // textBoxWidth
+            // 
+            this.textBoxWidth.Location = new System.Drawing.Point(88, 65);
+            this.textBoxWidth.Name = "textBoxWidth";
+            this.textBoxWidth.Size = new System.Drawing.Size(100, 20);
+            this.textBoxWidth.TabIndex = 1;
+            // 
+            // textBoxHeight
+            // 
+            this.textBoxHeight.Location = new System.Drawing.Point(88, 103);
+            this.textBoxHeight.Name = "textBoxHeight";
+            this.textBoxHeight.Size = new System.Drawing.Size(100, 20);
+            this.textBoxHeight.TabIndex = 2;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(88, 143);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(100, 23);
+            this.addButton.TabIndex = 3;
+            this.addButton.Text = "Add Object";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // sortButton
+            // 
+            this.sortButton.Location = new System.Drawing.Point(88, 184);
+            this.sortButton.Name = "sortButton";
+            this.sortButton.Size = new System.Drawing.Size(100, 23);
+            this.sortButton.TabIndex = 4;
+            this.sortButton.Text = "Sort by Volume";
+            this.sortButton.UseVisualStyleBackColor = true;
+            this.sortButton.Click += new System.EventHandler(this.sortButton_Click);
+            // 
+            // exportButton
+            // 
+            this.exportButton.Location = new System.Drawing.Point(88, 224);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(100, 23);
+            this.exportButton.TabIndex = 5;
+            this.exportButton.Text = "Export to File";
+            this.exportButton.UseVisualStyleBackColor = true;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // listBoxCharacteristics
+            // 
+            this.listBoxCharacteristics.FormattingEnabled = true;
+            this.listBoxCharacteristics.Location = new System.Drawing.Point(221, 28);
+            this.listBoxCharacteristics.Name = "listBoxCharacteristics";
+            this.listBoxCharacteristics.Size = new System.Drawing.Size(210, 264);
+            this.listBoxCharacteristics.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(452, 28);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 233);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelLength
+            // 
+            this.labelLength.AutoSize = true;
+            this.labelLength.Location = new System.Drawing.Point(24, 31);
+            this.labelLength.Name = "labelLength";
+            this.labelLength.Size = new System.Drawing.Size(46, 13);
+            this.labelLength.TabIndex = 8;
+            this.labelLength.Text = "Length: ";
+            // 
+            // labelWidth
+            // 
+            this.labelWidth.AutoSize = true;
+            this.labelWidth.Location = new System.Drawing.Point(24, 68);
+            this.labelWidth.Name = "labelWidth";
+            this.labelWidth.Size = new System.Drawing.Size(41, 13);
+            this.labelWidth.TabIndex = 9;
+            this.labelWidth.Text = "Width: ";
+            // 
+            // labelHeight
+            // 
+            this.labelHeight.AutoSize = true;
+            this.labelHeight.Location = new System.Drawing.Point(24, 106);
+            this.labelHeight.Name = "labelHeight";
+            this.labelHeight.Size = new System.Drawing.Size(44, 13);
+            this.labelHeight.TabIndex = 10;
+            this.labelHeight.Text = "Height: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(940, 489);
+            this.Controls.Add(this.labelHeight);
+            this.Controls.Add(this.labelWidth);
+            this.Controls.Add(this.labelLength);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBoxCharacteristics);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.sortButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.textBoxHeight);
+            this.Controls.Add(this.textBoxWidth);
+            this.Controls.Add(this.textBoxLength);
+            this.Name = "Form1";
+            this.Text = "Geometry App";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLength;
+        private System.Windows.Forms.TextBox textBoxWidth;
+        private System.Windows.Forms.TextBox textBoxHeight;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button sortButton;
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.ListBox listBoxCharacteristics;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelLength;
+        private System.Windows.Forms.Label labelWidth;
+        private System.Windows.Forms.Label labelHeight;
+    }
+}
---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp11
+{
+    // Класс Form1, представляющий главную форму приложения  
+    public partial class Form1 : Form
+    {
+        // Поле для хранения серии геометрических фигур  
+        private Series series = new Series();
+
+        // Конструктор для инициализации формы  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Обработчик события нажатия на кнопку "Добавить"  
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                double length = double.Parse(textBoxLength.Text);
+                double width = double.Parse(textBoxWidth.Text);
+                double height = double.Parse(textBoxHeight.Text);
+
+                Parallelepiped parallelepiped = new Parallelepiped(length, width, height);
+                series.AddBody(parallelepiped);
+
+                RefreshCharacteristicsDisplay();
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Please enter valid numeric values for length, width, and height.");
+            }
+        }
+
+        // Обработчик события нажатия на кнопку "Сортировать"  
+        private void sortButton_Click(object sender, EventArgs e)
+        {
+            series.SortByVolume();
+            RefreshCharacteristicsDisplay();
+        }
+
+        // Обработчик события нажатия на кнопку "Экспорт в файл"  
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog saveFileDialog = new SaveFileDialog())
+            {
+                saveFileDialog.Filter = "Text Files (*.txt)|*.txt";
+                if (saveFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    series.ExportToFile(saveFileDialog.FileName);
+                }
+            }
+        }
+
+        // Метод для обновления отображения характеристик в ListBox  
+        private void RefreshCharacteristicsDisplay()
+        {
+            listBoxCharacteristics.Items.Clear();
+
+            foreach (var body in series.GetBodies())
+            {
+                listBoxCharacteristics.Items.Add($"Surface Area: {body.SurfaceArea()}, Volume: {body.Volume()}");
+                body.Draw(pictureBox1); // Добавляем отрисовку фигуры на PictureBox  
+            }
+        }
+    }
+
+    // Абстрактный класс, представляющий геометрическое тело  
+    public abstract class Body
+    {
+        // Абстрактный метод для вычисления площади поверхности  
+        public abstract double SurfaceArea();
+
+        // Абстрактный метод для вычисления объема  
+        public abstract double Volume();
+
+        // Абстрактный метод для отрисовки фигуры на PictureBox  
+        public abstract void Draw(PictureBox pictureBox);
+    }
+
+    // Класс Parallelepiped, представляющий параллелепипед  
+    public class Parallelepiped : Body
+    {
+        // Поля для хранения размеров параллелепипеда  
+        private double Length { get; }
+        private double Width { get; }
+        private double Height { get; }
+
+        // Конструктор для инициализации размеров параллелепипеда  
+        public Parallelepiped(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        // Метод для вычисления площади поверхности параллелепипеда  
+        public override double SurfaceArea()
+        {
+            return 2 * (Length * Width + Length * Height + Width * Height);
+        }
+
+        // Метод для вычисления объема параллелепипеда  
+        public override double Volume()
+        {
+            return Length * Width * Height;
+        }
+
+        // Метод для отрисовки параллелепипеда на PictureBox  
+        public override void Draw(PictureBox pictureBox)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+            g.DrawRectangle(pen, 10, 10, (float)Length, (float)Width);
+        }
+    }
+
+    // Класс Cone, представляющий конус  
+    public class Cone : Body
+    {
+        // Поля для хранения радиуса и высоты конуса  
+        private double Radius { get; }
+        private double Height { get; }
+
+        // Конструктор для инициализации радиуса и высоты конуса  
+        public Cone(double radius, double height)
+        {
+            Radius = radius;
+            Height = height;
+        }
+
+        // Метод для вычисления площади поверхности конуса  
+        public override double SurfaceArea()
+        {
+            return Math.PI * Radius * (Radius + Math.Sqrt(Radius * Radius + Height * Height));
+        }
+
+        // Метод для вычисления объема конуса  
+        public override double Volume()
+        {
+            return (1.0 / 3.0) * Math.PI * Radius * Radius * Height;
+        }
+
+        // Метод для отрисовки конуса на PictureBox  
+        public override void Draw(PictureBox pictureBox)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+            g.DrawEllipse(pen, 10, 10, (float)Radius * 2, (float)Radius * 2);
+            g.DrawLine(pen, 10 + (float)Radius, 10 + (float)Radius * 2, 10 + (float)Radius, 10 + (float)Height);
+        }
+    }
+
+    // Класс Ball, представляющий сферу  
+    public class Ball : Body
+    {
+        // Поле для хранения радиуса сферы  
+        private double Radius { get; }
+
+        // Конструктор для инициализации радиуса сферы  
+        public Ball(double radius)
+        {
+            Radius = radius;
+        }
+
+        // Метод для вычисления площади поверхности сферы  
+        public override double SurfaceArea()
+        {
+            return 4 * Math.PI * Radius * Radius;
+        }
+
+        // Метод для вычисления объема сферы  
+        public override double Volume()
+        {
+            return (4.0 / 3.0) * Math.PI * Radius * Radius * Radius;
+        }
+
+        // Метод для отрисовки сферы на PictureBox  
+        public override void Draw(PictureBox pictureBox)
+        {
+            Graphics g = pictureBox.CreateGraphics();
+            Pen pen = new Pen(Color.Black);
+            g.DrawEllipse(pen, 10, 10, (float)Radius * 2, (float)Radius * 2);
+        }
+    }
+
+    // Класс Series, представляющий серию геометрических фигур  
+    public class Series
+    {
+        // Список для хранения геометрических фигур  
+        private List<Body> bodies = new List<Body>();
+
+        // Метод для добавления геометрической фигуры в серию  
+        public void AddBody(Body body)
+        {
+            bodies.Add(body);
+        }
+
+        // Метод для получения списка геометрических фигур  
+        public List<Body> GetBodies()
+        {
+            return bodies;
+        }
+
+        // Метод для сортировки геометрических фигур по объему  
+        public void SortByVolume()
+        {
+            bodies.Sort((a, b) => a.Volume().CompareTo(b.Volume()));
+        }
+
+        // Метод для экспорта информации о геометрических фигурах в файл  
+        public void ExportToFile(string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var body in bodies)
+                {
+                    writer.WriteLine($"Surface Area: {body.SurfaceArea()}, Volume: {body.Volume()}");
+                }
+            }
+        }
+
+        // Метод для импорта информации о геометрических фигурах из файла  
+        public void ImportFromFile(string filePath)
+        {
+            bodies.Clear();
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Парсинг строк и создание объектов тел тут...  
+                }
+            }
+        }
+    }
+}
